fb014d247375967a3385d6634eb33744bc622102
Eagle <wei.lee460@gmail.com>
2016-03-03 09:40:35 +0800

    Finish the block view creation. 2016-03-03

--- DropitDemo/DropitViewController.swift
+++ DropitDemo/DropitViewController.swift
@@ -12,5 +12,45 @@ class DropitViewController: UIViewController {
 
     @IBOutlet weak var gameView: UIView!
     
+    var dropsPerRow = 10
+    
+    var dropSize: CGSize {
+        let size = gameView.bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    @IBAction func drop(sender: UITapGestureRecognizer) {
+        drop()
+    }
+    
+    func drop()
+    {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        
+        gameView.addSubview(dropView)
+    }
 
+}
+
+private extension CGFloat {
+    static func random(max: Int) -> CGFloat {
+        //create a random float
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+private extension UIColor {
+    class var random: UIColor {
+        switch arc4random() % 5 {
+            case 0: return UIColor.greenColor()
+            case 1: return UIColor.blueColor()
+            case 2: return UIColor.orangeColor()
+            case 3: return UIColor.redColor()
+            case 4: return UIColor.purpleColor()
+            default: return UIColor.blackColor()
+        }
+    }
 }
